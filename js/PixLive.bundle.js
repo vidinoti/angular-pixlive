d7ab2800be0c1b27832e80e916ccc7d356b4db2b
Mathieu Monney <mathieu.monney@vidinoti.com>
2015-06-19 12:25:13 +0200

    Added missing SDK Controller in grunt file

--- js/PixLive.bundle.js
+++ js/PixLive.bundle.js
@@ -255,6 +255,47 @@ pixliveModule
  * angular-pixlive v0.0.1
  * (c) 2015 Vidinoti http://vidinoti.com
  * License: MIT
+ * 
+ * SDK Controller
+ *
+ */
+
+'use strict';
+
+pixliveModule
+    .factory('PxlController', [
+        '$ionicPlatform',
+        '$q',
+        function PxlController($ionicPlatform, $q) {
+
+            /*private*/
+
+            /*public*/
+            return {
+                presentNotificationsList: function() {
+                    var deferred = $q.defer();
+
+                    $ionicPlatform.ready(function () {
+                        if(window.cordova && window.cordova.plugins && window.cordova.plugins.PixLive) {
+                            window.cordova.plugins.PixLive.presentNotificationsList(function() {
+                                deferred.resolve();
+                            }, function() {
+                                deferred.reject();
+                            });
+                        } else {
+                            deferred.resolve([]);
+                        }
+                    });
+
+                    return deferred.promise;
+                }
+            };
+        }
+    ]);
+/*
+ * angular-pixlive v0.0.1
+ * (c) 2015 Vidinoti http://vidinoti.com
+ * License: MIT
  *
  * Event directives
  *
