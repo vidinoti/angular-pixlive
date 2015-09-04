a9994e1bd96b774289322972a0bc95f3883d82b2
Mathieu Monney <mathieu.monney@vidinoti.com>
2015-09-04 12:49:10 +0200

    Added method to retrive contexts

--- js/PixLive.bundle.js
+++ js/PixLive.bundle.js
@@ -326,6 +326,23 @@ pixliveModule
                     });
 
                     return deferred.promise;
+                },
+                getContexts: function() {
+                    var deferred = $q.defer();
+
+                    $ionicPlatform.ready(function () {
+                        if(window.cordova && window.cordova.plugins && window.cordova.plugins.PixLive) {
+                            window.cordova.plugins.PixLive.getContexts(function(list) {
+                                deferred.resolve(list);
+                            }, function() {
+                                deferred.reject();
+                            });
+                        } else {
+                            deferred.resolve([]);
+                        }
+                    });
+
+                    return deferred.promise;
                 }
             };
         }
